6c1089740a38ab22291f71b27b6293e0b8a68baa
Vadim Mishakov <lucheboche@gmail.com>
2021-10-09 13:18:44 +0600

    L5 Review fixes. Add Scss & BEM to Modal

--- src/js/main.js
+++ src/js/main.js
@@ -48,23 +48,23 @@ let innerHtmlText = '';
 
 cardsMassive.forEach((el) => {
     innerHtmlText += `
-<div class="cards-item">
-<div class="cards-item__image">
+<div class="card-item">
+<div class="card-item__image">
     <img src="${el.fotoUrl}" alt="${el.fotoAlt}" width="330" height="330">
-    ${el.newItem ? `<div class="cards-item__stick">
+    ${el.newItem ? `<div class="card-item__stick">
         new
     </div>` : ''}
 </div>
-<div class="cards-item__price">
+<div class="card-item__price">
     ${el.priceItem} баллов
 </div>
-<div class="cards-item__title">
+<div class="card-item__title">
 ${el.titleItem}
 </div>
-<div class="cards-item__sizes">
+<div class="card-item__sizes">
 ${el.sizesItem}
 </div>
-<button class="btn cards-item__btn">
+<button class="btn card-item__btn">
     Заказать
 </button>
 </div>
