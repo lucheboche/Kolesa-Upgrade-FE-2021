57ba76c13154d1a57a769d90a03c8a61fca5663e
Vadim Mishakov <lucheboche@gmail.com>
2021-11-09 13:25:57 +0600

    refactor, add divs to index.html

--- src/js/bigUntestableCode.js
+++ src/js/bigUntestableCode.js
@@ -1,47 +1,34 @@
-import { getItemsRequest, toggleFavoriteRequest } from './requests';
+import {
+    getItemsRequest,
+    showElement,
+    offElement,
+    runButtonElements,
+} from './requests';
+
+const errorElement = document.querySelector('#error');
+const appElement = document.querySelector('#app');
+const loaderElement = document.querySelector('#loader');
 
 export default () => {
-    document.querySelector('#error').style.display = 'none';
-    document.querySelector('#loader').style.display = 'block';
+    offElement(errorElement);
+    showElement(loaderElement);
 
     getItemsRequest()
         .then(({ data }) => {
             if (data.result !== 'ok' || typeof data.html === 'undefined') {
-                const errorElement = document.querySelector('#error');
-
                 errorElement.innerHTML = 'Произошла ошибка, попробуйте ещё раз.';
-                errorElement.style.display = 'block';
+                showElement(errorElement);
             } else {
-                const appElement = document.querySelector('#app');
-
                 appElement.innerHTML = data.html;
-                appElement.style.display = 'block';
-
-                Array.from(appElement.querySelector('button')).forEach((button) => {
-                    button.addEventListener('click', (e) => {
-                        e.preventDefault();
-
-                        e.currentTarget.textContent = 'Загрузка...';
-
-                        toggleFavoriteRequest(e.currentTarget.dataset.id)
-                            .then(({ data: buttonData }) => {
-                                if (buttonData.result === 'set') {
-                                    e.currentTarget.textContent = '🌝';
-                                } else {
-                                    e.currentTarget.textContent = '🌚';
-                                }
-                            });
-                    });
-                });
+                showElement(appElement);
+                runButtonElements(appElement);
             }
         })
         .catch((e) => {
-            const errorElement = document.querySelector('#error');
-
             errorElement.innerHTML = e.message;
-            errorElement.style.display = 'block';
+            showElement(errorElement);
         })
         .finally(() => {
-            document.querySelector('#loader').style.display = 'none';
+            offElement(loaderElement);
         });
 };
--- src/js/requests.js
+++ src/js/requests.js
@@ -5,3 +5,34 @@ axios.defaults.baseURL = 'http://example.com';
 export const getItemsRequest = () => axios.get('/getItems');
 
 export const toggleFavoriteRequest = id => axios.post('/toggleFavorite', new URLSearchParams({ id }));
+
+export function showElement(e) {
+    e.style.display = 'block';
+}
+
+export function offElement(e) {
+    e.style.display = 'none';
+}
+
+export function buttonsListener(e) {
+    e.preventDefault();
+
+    e.currentTarget.textContent = 'Загрузка...';
+
+    toggleFavoriteRequest(e.currentTarget.dataset.id)
+        .then(({ data: buttonData }) => {
+            if (buttonData.result === 'set') {
+                e.currentTarget.textContent = '🌝';
+            } else {
+                e.currentTarget.textContent = '🌚';
+            }
+        });
+}
+
+export function runButtonElements(e) {
+    const buttonElements = Array.from(e.querySelector('button'));
+
+    buttonElements.forEach((button) => {
+        button.addEventListener('click', el => buttonsListener(el));
+    });
+}
